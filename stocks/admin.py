485475f12f20e303895d8a99264da1d6ce66e218
kisumutalent <homiemusa@gmail.com>
2020-08-17 16:29:35 +0300

    Create and customise Admin

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Stock
+from .forms import StockCreateForm
+
+class StockCreateAdmin(admin.ModelAdmin):
+	list_display = ['category', 'item_name', 'quantity']
+	form = StockCreateForm
+	list_filter = ['category']
+	search_fields = ['category', 'item_name']
+admin.site.register(Stock, StockCreateAdmin)
