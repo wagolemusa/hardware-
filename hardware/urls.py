0567f4160f0f4c3bd00d16eab7871665ffc274a6
kisumutalent <homiemusa@gmail.com>
2020-08-18 18:38:17 +0300

    Worked on url

--- hardware/urls.py
+++ hardware/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stocks import views
 
 urlpatterns = [
+		path('', views.home, name='home'),
+		path('list_items/', views.list_items, name='list_names'),
     path('admin/', admin.site.urls),
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Stock
+
+def home(request):
+	title = "Welcome : this to the store"
+
+	context = {
+	"title" : title
+	}
+	return render(request, "home.html", context)
+
+def list_items(request):
+	querySet = Stock.objects.all()
+
+	context = {
+		"querySet": querySet
+	}
+
+	return render(request, "list_items.html", context)
