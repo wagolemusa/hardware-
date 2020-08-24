e8206f5ec7a9f2c971a7467ac7f15fc249e7bee8
kisumutalent <homiemusa@gmail.com>
2020-08-24 07:16:35 +0300

    Create all changes

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Stock
+from .models import Stock, Category
 from .forms import StockCreateForm
 
 class StockCreateAdmin(admin.ModelAdmin):
@@ -9,4 +9,7 @@ class StockCreateAdmin(admin.ModelAdmin):
 	form = StockCreateForm
 	list_filter = ['category']
 	search_fields = ['category', 'item_name']
+
+
 admin.site.register(Stock, StockCreateAdmin)
+admin.site.register(Category)
--- stocks/models.py
+++ stocks/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 # Create your models here.
+category_choice = (
+			('Furniture', 'furniture'),
+			('IT Equipment', 'IT equirement'),
+			('Phone', 'phone'),
+			('Electronic', 'Phone')
+	)
+
+class Category(models.Model):
+	name = models.CharField(max_length=50, blank=True, null=True)
+	def __str__(self):
+		return self.name
 
 class Stock(models.Model):
-	category = models.CharField(max_length=50, blank=True, null=True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	item_name = models.CharField(max_length=50, blank=True, null=True)
-	quantity = models.IntegerField(default='0', blank=True, null=True)
+	quantity = models.IntegerField(default='0', blank=False, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_by = models.CharField(max_length=50, blank=True, null=True)
 	issue_quantity = models.IntegerField(default='0', blank=True, null=True)
@@ -15,7 +26,9 @@ class Stock(models.Model):
 	created_by = models.CharField(max_length=50, blank=True, null=True)
 	reoder_level = models.IntegerField(default='0', blank=True, null=True)
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-	export_to_csv = models.BooleanField(default=False)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	# date = models.DateTimeField(auto_now_add=False, auto_now=False)
+	# export_to_csv = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.item_name + ' ' + str(self.quantity)
